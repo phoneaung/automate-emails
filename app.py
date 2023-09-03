69f15393a6016c7026b486a58fc6e498d9c86cd3
Phone Aung <phonexaung@gmail.com>
2023-09-03 15:41:50 +0630

    Add port number and servers

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import smtplib
 from email.message import EmailMessage
 from email.utils import formataddr
 from pathlib import Path
+
+# pip install pandas python-dotenv
+from dotenv import load_dotenv
+
+# Outlook port number and smtp server. This may varies depending on Mail Server that you use.
+PORT = 587
+EMAIL_SERVER = 'smtp-mail.outlook.com'
+
+
