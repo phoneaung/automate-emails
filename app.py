f8c1789488442e400b8bb8c763bd358cbf90a287
Phone Aung <phonexaung@gmail.com>
2023-09-03 15:53:53 +0630

    add comments

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from pathlib import Path
 # pip install python-dotenv
 from dotenv import load_dotenv
 
-# outlook port number and smtp server. This may varies depending on Mail Server that you use.
+# outlook port number and smtp server. This may varies depending on Mail Server that you use. Adjust port if you use SSL.
 PORT = 587
 EMAIL_SERVER = 'smtp-mail.outlook.com'
 
-# load the environment variables
+# load the environment variables from .env file 
+# included if statement so that it will also work in Jupyter Notebook
 current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
 envars = current_dir / ".env"
 load_dotenv(envars)
@@ -19,5 +20,4 @@ load_dotenv(envars)
 # read the environment variables
 sender_email = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
-
 
